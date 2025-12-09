ae7b86ebd1ad2b51a64bc1444ba9db79e2735c38
john2772737 <88893071+john2772737@users.noreply.github.com>
2025-12-09 18:01:57 +0800

    SDF

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ import {
   LockIcon,
   BellIcon,
 } from "../utils/icons";
-import AICard from "./AICard"; 
+import AICard from "../components/AICard"; 
 import { useAuth } from '../context/AuthContext'; // Needed for AlertSettingsComponent
 
 const API_ENDPOINT = "https://baha-alert.vercel.app/api";
